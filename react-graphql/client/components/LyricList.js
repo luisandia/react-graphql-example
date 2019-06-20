94ae089418c0b17aabe7ea33d036681512ba7c59
luisandia <luisandiamamani@gmail.com>
2019-06-20 01:26:18 +0000

    add like function

--- react-graphql/client/components/LyricList.js
+++ react-graphql/client/components/LyricList.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 
 class LyricList extends Component {
 
-    onLike(id) {
-        console.log(id);
+    onLike(id, likes) {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        });
     }
-
-
     renderLyrics() {
-        console.log(this.props.lyrics)
-        return this.props.lyrics.map(({ id, content }) => {
+        return this.props.lyrics.map(({ id, content, likes }) => {
             return (
-                <li key={id} className="collection-item"> {content}
-                    <i
-                        onClick={() => this.onLike(id)}
-                        className="material-icons">thumb_up</i>
+                <li key={id} className="collection-item">
+                    {content}
+                    <div className="vote-box">
+                        <i
+                            className="material-icons"
+                            onClick={() => this.onLike(id, likes)}
+                        >
+                            thumb_up
+            </i>
+                        {likes}
+                    </div>
+
                 </li>
             );
         });
@@ -26,8 +43,17 @@ class LyricList extends Component {
             <ul className="collection">
                 {this.renderLyrics()}
             </ul>
-        )
+        );
     }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID){
+    likeLyric(id: $id){
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
--- react-graphql/client/queries/fetchSong.js
+++ react-graphql/client/queries/fetchSong.js
@@ -9,6 +9,7 @@ query SongQuery($id:ID!){
       lyrics{
           id
           content
+          likes
       }
     }
   }`;
